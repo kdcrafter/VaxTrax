25fce46beaff42da4639debe0958c9b0f1ae5c91
Kurtis Dressel <kurtisdoubled@hotmail.com>
2021-02-24 09:48:55 -0500

    added bad test

--- tests/test_example.py
+++ tests/test_example.py
@@ -3,7 +3,7 @@ import dash_html_components as html
 
 from us_today import us_layout
 
-def test_us001_example(dash_duo):
+def test_us001_simple_example(dash_duo):
     app = dash.Dash(__name__)
     app.layout = us_layout
     dash_duo.start_server(app)
@@ -11,3 +11,6 @@ def test_us001_example(dash_duo):
     dash_duo.wait_for_element_by_id('us-total-vaccination-today')
 
     dash_duo.percy_snapshot("us001_example")
+
+def test_us001_fail_example(dash_duo):
+    assert False
