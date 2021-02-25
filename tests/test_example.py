1675413aff726592b5548b8f8b99d30dbc86ebab
Kurtis Dressel <kurtisdoubled@hotmail.com>
2021-02-24 21:56:21 -0500

    test graph

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,13 +1,37 @@
 import dash
+import dash_core_components as dcc
 import dash_html_components as html
+import plotly.express as px
+import pandas as pd
 
 from us_today import us_layout
 
 def test_us001_simple_example(dash_duo):
-    app = dash.Dash(__name__)
-    app.layout = html.Div([
-        html.H1('Hello'),
+    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+    app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+    df = pd.DataFrame({
+        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+    })
+
+    fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+    app.layout = html.Div(children=[
+        html.H1(children='Hello Dash'),
+
+        html.Div(children='''
+            Dash: A web application framework for Python.
+        '''),
+
+        dcc.Graph(
+            id='example-graph',
+            figure=fig
+        )
     ])
+
     dash_duo.start_server(app)
 
     dash_duo.percy_snapshot('us001_example')
