630e8d4b06fa149cca5aaf8a6d1ac71f23f64679
Kurtis Dressel <kurtisdoubled@hotmail.com>
2021-02-24 21:51:17 -0500

    added hello

--- tests/test_example.py
+++ tests/test_example.py
@@ -5,9 +5,9 @@ from us_today import us_layout
 
 def test_us001_simple_example(dash_duo):
     app = dash.Dash(__name__)
-    app.layout = us_layout
+    app.layout = html.Div([
+        html.H1('Hello'),
+    ])
     dash_duo.start_server(app)
 
-    dash_duo.wait_for_element_by_id('us-total-vaccination-today')
-
-    dash_duo.percy_snapshot("us001_example")
+    dash_duo.percy_snapshot('us001_example')
